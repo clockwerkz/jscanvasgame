8a156a52d611a1f06e3a361af7da0adeef74cad3
Carlos <clockwerkz@gmail.com>
2021-05-29 16:55:49 -0700

    adds enemy class

--- js/app.js
+++ js/app.js
@@ -7,6 +7,21 @@ const CANVAS_WIDTH = 500;
 
 class Enemy {
     constructor(){
-        
+        this.x = 10;
+        this.y = 50,
+        this.width = 50,
+        this.height = 50
     }
 }
+
+const enemy1 = new Enemy();
+
+function animate() {
+    ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemy1.x++;
+    enemy1.y++;
+    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    requestAnimationFrame(animate);
+}
+
+animate();
