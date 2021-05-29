52c5aca79a086afe431f56be9c52026426609d22
Carlos <clockwerkz@gmail.com>
2021-05-29 16:51:12 -0700

    basic canvas set up

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/**   @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+const CANVAS_HEIGHT = 850;
+const CANVAS_WIDTH = 500;
+
+class Enemy {
+    constructor(){
+        
+    }
+}
