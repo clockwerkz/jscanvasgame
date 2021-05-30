5a422ea813b6142eb7d93d48fa5a7a7b2b0370cc
Carlos <clockwerkz@gmail.com>
2021-05-29 23:28:40 -0700

    vert sine wave movement

--- js/app.js
+++ js/app.js
@@ -11,25 +11,31 @@ canvas.setAttribute('width', CANVAS_WIDTH);
 
 
 let gameFrame = 0;
-
+///Enemy 1: Width 293, Height 155
+//Enemy 2: Width 266, Height 188
 class Enemy {
     constructor(){
         this.image = new Image();
-        this.image.src = '../images/enemy1.png';
-        this.spriteWidth = 293;
-        this.spriteHeight = 155;
+        this.image.src = '../images/enemy2.png';
+        this.spriteWidth = 266;
+        this.spriteHeight =188;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
-            this.y = Math.random() * (canvas.height - this.height);
-        this.speed = Math.random() * 4 - 2;
+        this.y = Math.random() * (canvas.height - this.height);
+        this.speed = Math.random() * 4 + 1;
         this.frame = 0;
         this.flapSpeed = parseInt(Math.random() * 3)  + 1;
+        this.angle = Math.random() * 2;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 4 + 1;
     }
     update(){
-        this.x+=Math.random() * 4 - 2;
-        this.y+=Math.random() * 6 - 3;
+        this.x -= this.speed;
+        this.y+= this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
         //update sprites
+        if (this.x + this.width < 0) this.x = canvas.width;
         if (gameFrame %  this.flapSpeed == 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
